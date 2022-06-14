85f96accaf69a19362bfe4656ce68a80d202285c
emaibb <mzfengibb@outlook.com>
2022-06-14 11:05:07 +0800

    CMake

--- cmake/FindBAMJL.cmake
+++ cmake/FindBAMJL.cmake
@@ -4,24 +4,24 @@ find_path(BAMJL_ROOT_DIR
     NAMES include/BAMJL.h
 )
 
-find_path(BAMJL_INCLUDE_DIR
+find_path(BAMJL_INCLUDE_DIRS
     NAMES BAMJL.h
-    HINTS ${BAM_ROOT_DIR}/include
+    HINTS ${BAMJL_ROOT_DIR}/include
 )
 
 find_library(BAMJL_LIBRARY
     NAMES BAMJL
-    HINTS ${BAM_ROOT_DIR}/lib
+    HINTS ${BAMJL_ROOT_DIR}/lib
 )
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(BAM DEFAULT_MSG
-    BAMJL_INCLUDE_DIR
+find_package_handle_standard_args(BAMJL DEFAULT_MSG
+    BAMJL_INCLUDE_DIRS
     BAMJL_LIBRARY
 )
 
 mark_as_advanced(
     BAMJL_ROOT_DIR
-    BAMJL_INCLUDE_DIR
+    BAMJL_INCLUDE_DIRS
     BAMJL_LIBRARY
 )
